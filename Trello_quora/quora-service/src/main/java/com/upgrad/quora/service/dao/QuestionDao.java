1cc3a0497f4f5802c0d66ebced3a6f49a364a444
KetaEs <KetaEs@dev.azure.com>
2021-03-04 14:52:38 +0100

    added question controller business dao with and create api

---
+++ Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
@@ -0,0 +1,23 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.QuestionEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class QuestionDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public QuestionEntity createQuestion(QuestionEntity questionEntity) {
+        entityManager.persist(questionEntity);
+        return questionEntity;
+    }
+
+
+}
